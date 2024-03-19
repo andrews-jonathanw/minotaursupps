74a3981e894bf6576a130e9208e2baeeca8ad274
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-03-18 19:40:13 -0500

    Add Firebase dependencies and initialize Firebase app

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,44 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+import { getFirestore, getDocs, collection } from 'firebase/firestore';
+import '../firebase/firebase';
 
-export default function Home() {
+const Home = () => {
+  const [data, setData] = useState(null);
+  const firestore = getFirestore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, 'test'));
+        const documentsData = querySnapshot.docs.map(doc => doc.data());
+        if (documentsData.length > 0) {
+          setData(documentsData[0]);
+        } else {
+          console.log('No documents found in the collection!');
+        }
+      } catch (error) {
+        console.error('Error fetching documents:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Fuel Your Inner Beast
+        {data ? (
+          <div>
+            <h1>Fuel Your Inner Beast</h1>
+
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </main>
   );
-}
+};
+
+export default Home;
