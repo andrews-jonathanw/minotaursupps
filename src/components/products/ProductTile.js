26147eae159911344afabc1adbdfa750fdb33455
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-04-24 18:03:29 -0500

    Add motion effects to ProductTile component

--- src/components/products/ProductTile.js
+++ src/components/products/ProductTile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function ProductTile({ product }) {
 
@@ -7,12 +8,21 @@ export default function ProductTile({ product }) {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center w-64 h-auto p-4 gap-8' onClick={navigateToProductPage}>
-      <img src={'/assets/supplementImages/23336.jpg'} alt={product.name} className='w-64 h-64 object-cover' />
+    <motion.div
+      className='flex flex-col items-center justify-center w-64 h-auto p-4 gap-8 cursor-pointer'
+      onClick={navigateToProductPage}
+      whileHover={{ scale: 1.05, transition: { duration: 0.8 } }}
+    >
+      <motion.img
+        src={'/assets/supplementImages/23336.jpg'}
+        alt={product.name}
+        className='w-64 h-64 object-cover'
+        transition={{ duration: .6 }}
+      />
       <div className='text-center'>
         <p className='text-sm font-semibold mb-1 text-white'>{product.name}</p>
-        <p className='text-sm  text-white'>Price: ${product.prices[0].unit_amount / 100}</p>
+        <p className='text-sm text-white'>Price: ${product.prices[0].unit_amount / 100}</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
