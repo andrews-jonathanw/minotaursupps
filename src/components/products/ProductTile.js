73328fc75f6549115551dec6c1f9ca7f5c6adb12
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-04-24 17:53:07 -0500

    Added temp image for supplement products

--- src/components/products/ProductTile.js
+++ src/components/products/ProductTile.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function ProductTile({ product }) {
+  console.log(product);
   return (
     <div className='flex flex-col items-center justify-center w-64 h-auto p-4 gap-8'>
-      <img src={product.metadata.img} alt={product.metadata.name} className='w-64 h-64 object-cover' />
+      <img src={'/assets/supplementImages/23336.jpg'} alt={product.metadata.name} className='w-64 h-64 object-cover' />
       <div className='text-center'>
         <p className='text-sm font-semibold mb-1 text-white'>{product.name}</p>
         <p className='text-sm  text-white'>Price: ${product.prices[0].unit_amount / 100}</p>
