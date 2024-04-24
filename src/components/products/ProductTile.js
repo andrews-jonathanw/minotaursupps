6b298c3c7d74f979d6338527a8b985fe6843d278
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-04-24 17:58:35 -0500

    Update product links to include category in URL.

--- src/components/products/ProductTile.js
+++ src/components/products/ProductTile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default function ProductTile({ product }) {
-  console.log(product);
+
+  const navigateToProductPage = () => {
+    window.location.href = `/products/${product.metadata.category}/${product.name}`;
+  }
+
   return (
-    <div className='flex flex-col items-center justify-center w-64 h-auto p-4 gap-8'>
-      <img src={'/assets/supplementImages/23336.jpg'} alt={product.metadata.name} className='w-64 h-64 object-cover' />
+    <div className='flex flex-col items-center justify-center w-64 h-auto p-4 gap-8' onClick={navigateToProductPage}>
+      <img src={'/assets/supplementImages/23336.jpg'} alt={product.name} className='w-64 h-64 object-cover' />
       <div className='text-center'>
         <p className='text-sm font-semibold mb-1 text-white'>{product.name}</p>
         <p className='text-sm  text-white'>Price: ${product.prices[0].unit_amount / 100}</p>
